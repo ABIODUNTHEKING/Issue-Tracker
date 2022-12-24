aca5b63d0d192bbc87f9ab2b514f3945c31f4061
Emmanuel Abiodun Akinjogunla <105178231+ABIODUNTHEKING@users.noreply.github.com>
2022-12-24 23:22:23 +0100

    DONE

--- script.js
+++ script.js
@@ -1,61 +1,90 @@
-function newIssue(){
-    let mainissues = JSON.parse(localStorage.getItem('issues'))
-    let receivedIssues = document.getElementById('received-issues')
+let form = document.getElementById('form');
+form.addEventListener('submit', saveIssue);
 
-    receivedIssues.innerHTML= ""
+function saveIssue(e){
+  e.preventDefault()
 
-    for(let i = 0; i < mainissues.length; i++){
-        let id = mainissues[i].id;
-        let user = mainissues[i].user;
-        let description = mainissues[i].description;
-        let fixer = mainissues[i].fixer;
-        let priority = mainissues[i].priority;
-        let date = mainissues[i].date;
-        let status = mainissues[i].status;
-    
 
-    receivedIssues.innerHTML = `<div> 
-                               <h6>The ISSUE ID: ${id} created on ${date} by ${user}</h6> 
-                               <p class="text-center text-info"> ${status} </p>
-                               <h3>${description}</h3>
-                               <p><i class="fa-regular fa-business-time"></i>${priority} <i class="fa-regular fa-user"></i>${fixer}</p>
-                               <a href="#" class="btn btn-warning" onclick="issueResolved(${issueId})">Close</a> 
-                               <a href="#" class="btn btn-danger" onclick="deleteIssue(${issueId})">Delete</a>
-                               </div>`;
-    }
+  let issue = {
+      id: chance.guid(),
+      description: document.getElementById('issue').value,
+      severity: document.getElementById('priority').value,
+      assignedTo: document.getElementById('assigned-to').value,
+      status: 'Open',
+      issuedBy: document.getElementById('issuer').value,
+      date: document.getElementById('date').value 
+  }
+
+  if(localStorage.getItem('issues') === null){
+    let setOfIssues = []
+    setOfIssues.push(issue)
+    localStorage.setItem('issues', JSON.stringify(setOfIssues))
+  }
+  else{
+    let setOfissues = JSON.parse(localStorage.getItem('issues'))
+    setOfissues.push(issue)
+    localStorage.setItem('issues', JSON.stringify(setOfissues))
+  }
+
+  form.reset();
+  newIssue();
+}
+
+
+function newIssue(){ 
+  let mainIssues = JSON.parse(localStorage.getItem('issues'))
+  let receivedIssues = document.getElementById('received-issues')
+
+  receivedIssues.innerHTML = '';
+
+  for(let i = 0; i < mainIssues.length; i++){
+    let id = mainIssues[i].id;
+    let user = mainIssues[i].user;
+    let description = mainIssues[i].description;
+    let fixer = mainIssues[i].fixer;
+    let priority = mainIssues[i].priority;
+    let date = mainIssues[i].date;
+    let status = mainIssues[i].status;
+  
+  receivedIssues.innerHTML +=   '<div class="well bg-white shadow rounded mt-2 mx-2 py-3 px-5">'+
+                                '<h6>Issue ID: ' + id + '</h6>'+
+                                '<p><span class="label label-info">' + status + '</span></p>'+
+                                '<h3>' + description + '</h3>'+
+                                '<p>Issued by ' + user + ' on ' + date + '</p>' +
+                                '<p><i class="fa-solid fa-business-time"></i> ' + priority + ' '+
+                                '<i class="fa-regular fa-user"></i> ' + fixer + '</p>'+
+                                '<a href="#" class="btn btn-success" onclick="closeIssue(\''+id+'\')">Close</a> '+
+                                '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+                                '</div>';
+  }
 
 }
 
-document.getElementById('form').addEventListener('submit', saveIssue);
-
-function saveIssue(e) {
-    let issueId = chance.guid();
-    let issueDescription = document.getElementById('issue').value;
-    let issuePriority = document.getElementById('priority').value;
-    let issueFixer = document.getElementById('assigned-to').value;
-    let issueStatus = 'Open';
-    let issue = {
-      id: issueId,
-      description: issueDescription,
-      severity: issuePriority,
-      assignedTo: issueFixer,
-      status: issueStatus
+function closeIssue(id){
+  let issues = JSON.parse(localStorage.getItem('issues'))
+  for(let i = 0; i < issues.length; i++){
+    if(issues[i].id == id){
+      issues[i].status = "Close"
     }
-    
-    if (localStorage.getItem('issues') === null) {
-      var issues = [];
-      issues.push(issue);
-      localStorage.setItem('issues', JSON.stringify(issues));
-    } else {
-      var issues = JSON.parse(localStorage.getItem('issues'));
-      issues.push(issue);
-      localStorage.setItem('issues', JSON.stringify(issues));
-    }
-    
-    document.getElementById('issueInputForm').reset();
-   
-    newIssue();
-    
-    e.preventDefault(); 
   }
+  localStorage.setItem('issues', JSON.stringify(issues));
+  newIssue()
+}
+
+function deleteIssue(id){
+  let issues = JSON.parse(localStorage.getItem('issues'))
+  for(let i = 0; i < issues.length; i++){
+    if(issues[i].status == "Close"){
+      issues.splice(i, 1)
+ 
+    }
+  }
+  localStorage.setItem('issues', JSON.stringify(issues));
+  newIssue()
+}
+
+
+
+
+
 
